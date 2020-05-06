7e683e0adbc482ac40e7ee68ecb0c7705c5fe603
Simhadri Sirisha <sirishasimhadri5@gmail.com>
2020-05-06 17:54:49 +0530

    Updates

---
+++ precache-manifest.354179b5da43b07ca6c53d87642c66ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2373e2608d1d5b83938e45cd7d30811",
+    "url": "/Robo-Friends/index.html"
+  },
+  {
+    "revision": "19712845b0f679a0b205",
+    "url": "/Robo-Friends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "87672c0e6ee9d73412ba",
+    "url": "/Robo-Friends/static/css/main.d6144945.chunk.css"
+  },
+  {
+    "revision": "19712845b0f679a0b205",
+    "url": "/Robo-Friends/static/js/2.de1efd81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Friends/static/js/2.de1efd81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87672c0e6ee9d73412ba",
+    "url": "/Robo-Friends/static/js/main.a2cbdbba.chunk.js"
+  },
+  {
+    "revision": "371c20676ed93a4dbca4",
+    "url": "/Robo-Friends/static/js/runtime-main.271714bd.js"
+  }
+]);
